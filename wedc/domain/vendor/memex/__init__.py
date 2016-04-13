7c0bc880c1bddd26190e82f9c9378204a3f81eed
ZwEin <zwein27@gmail.com>
2016-04-12 21:30:50 -0700

    update README

--- wedc/domain/vendor/memex/__init__.py
+++ wedc/domain/vendor/memex/__init__.py
@@ -6,3 +6,8 @@
 
 # https://memexproxy.com/wiki/display/MS/Memex+Cluster+and+Systems
 
+# http://10.1.94.54:8888/filebrowser/view=/user/worker/hbase-dump-2-months-19feb/webpage/part-00000?mode=text&compression=none
+
+
+# http://10.1.94.54:8888/filebrowser/view=/user/worker/hbase-dump-2-months-19feb/webpage
+
