df2128c78283f53d5e187bc83f3370cabf2bcaf6
ZwEin <zwein27@gmail.com>
2016-05-16 11:10:07 -0700

    filter numbers in string form

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # WEDC
 
+## Todo List
+
+## Change Log
 
 
-## Tasks
 
-1. use word2vec on new dataset by PySpark to get model
-2. get 2 level similar words for seed words
-3. adjust weight of seed word by comparing model result of Google News
-    1. seed words appear in both model have higher weight
-4. post match seed words, post2vec, vector is based on seed words
-5. build knn or LSH graph
-    1. sklearn library knn
-6. use label propagation
+
+
+
+
+
+
+
+
+
+
+
+
--- wedc/domain/core/common/stopword_helper.py
+++ wedc/domain/core/common/stopword_helper.py
@@ -7,11 +7,18 @@ def get_stopword_set():
     stops = stopword.get_stopwords()
     names = get_person_names()
     # country, country_abbr = stopword_helper.get_country_names()
+    numbers = get_numbers()
     nationality = get_nationality_names()
-    stop_set = set(stops) | set(names) | set(nationality) # | Set(country) # | Set(country_abbr) 
+    stop_set = set(stops) | set(numbers) | set(names) | set(nationality) # | Set(country) # | Set(country_abbr) 
     # stop_set = [str(stem.stemming(_).strip()) for _ in stop_set]
     return stop_set
 
+def get_numbers():
+    numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+                'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
+                'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',
+                'hundred', 'thousand', 'million', 'billion']
+    return numbers
 
 
 def get_stopword_from_file(file_path):
