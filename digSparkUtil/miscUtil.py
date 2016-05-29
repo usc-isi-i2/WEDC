1625b15c235285b021cdf59a368f56c504118712
ZwEin <zwein27@gmail.com>
2016-05-29 14:46:22 -0700

    remove digSparkUtil and install by pip

--- digSparkUtil/miscUtil.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# Filename: miscUtil.py
-
-from datetime import datetime
-
-def seconds_since_epoch():
-    return int(round((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()))
